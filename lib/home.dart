3adcdd37f7371af35e976e7df1fe80134016ec63
nandansudheesh <nandansudheesh1@gmail.com>
2025-07-11 17:23:41 +0530

    Initial commit

---
+++ lib/home.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:whatsapp_ui_clone/calls.dart';
+import 'package:whatsapp_ui_clone/chats.dart';
+import 'package:whatsapp_ui_clone/community.dart';
+import 'package:whatsapp_ui_clone/updates.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int index = 0;
+  List<Widget> pages = [Chats(), Updates(), Community(), Calls()];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        foregroundColor: Colors.white,
+        title: Text("Whatsapp"),
+        actions: [
+          Icon(Icons.qr_code_scanner),
+          SizedBox(width: 25),
+          Icon(Icons.camera_alt),
+          SizedBox(width: 25),
+          Icon(Icons.more_vert),
+          SizedBox(width: 20),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.chat),
+        backgroundColor: Colors.green,
+        foregroundColor: Colors.black,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: index,
+        onTap: (value) {
+          setState(() {
+            index=value;
+          });
+        },
+        selectedItemColor: Colors.green,unselectedItemColor: Colors.grey,
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.chat), label: "Chats",backgroundColor: Colors.black),
+          BottomNavigationBarItem(icon: Icon(Icons.update), label: "Updates"),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.people_outline),
+            label: "Community",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.pending_actions_outlined),
+            label: "Calls",
+          ),
+        ],
+      ) ,body: pages[index],
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:whatsapp_ui_clone/home.dart';
+
+void main() {
+  runApp(MaterialApp(debugShowCheckedModeBanner: false,home: Home(),));
+}
